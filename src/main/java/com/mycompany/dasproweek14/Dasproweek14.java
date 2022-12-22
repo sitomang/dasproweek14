6eec522ef59f797ae63493ffe839db37cf909b9e
YOGA 260 <YOGA 260@kareem>
2022-12-22 10:46:24 +0700

    commit awal

---
+++ src/main/java/com/mycompany/dasproweek14/Dasproweek14.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dasproweek14;
+
+/**
+ *
+ * @author YOGA 260
+ */
+public class Dasproweek14 {
+
+    public static void main(String[] args) {
+        loginview lv = new loginview();
+        lv.setLocationRelativeTo(null);
+        lv.setVisible(true);
+    }
+}
